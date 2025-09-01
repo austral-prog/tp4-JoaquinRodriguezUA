1c2df3872b99ba9c52f370c4c048876cc6636204
JoaquinRodriguezUA <jrodriguez11@mail.austral.edu.ar>
2025-09-01 12:39:54 -0300

    Add files via upload

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,12 @@
 def leap_year():
-    print("TO DO")
+    año = int(input("Ingrese un año: "))
+    if (año%100 == 0) and (año%400 == 0):
+        print("El año",año,"es bisiesto")
+    elif (año%100 == 0) and (año%400 != 0):
+        print("El año",año,"no es bisiesto")
+    elif (año%4 == 0):
+        print("El año",año,"es bisiesto")
+    else:
+        print("El año",año,"no es bisiesto")
+
+# leap_year()
--- line.py
+++ line.py
@@ -1,2 +1,26 @@
+import math
+
 def line():
-    print("TO DO")
+    ca = float(input("Ingrese el coeficiente A: "))
+    cb = float(input("Ingrese el coeficiente B: "))
+    cx1 = float(input("Ingrese el coeficiente X1: "))
+    cx2 = float(input("Ingrese el coeficiente X2: "))
+    
+    p1 = (cx1, (ca*cx1+cb))
+    p2 = (cx2, (ca*cx2+cb))
+
+    distancia = math.dist(p1,p2)
+
+    print("El coeficiente A de su ecuación de la recta es:", ca)
+    print("El coeficiente B de su ecuación de la recta es:", cb)
+    print("El coeficiente X1 de su ecuación de la recta es:", cx1)
+    print("El coeficiente X2 de su ecuación de la recta es:", cx2)
+    print("")
+    print("Para la siguiente ecuación:")
+    print("\tY = "+str(ca)+"X + "+str(cb)+"")
+    print("")
+    print("Dados los siguientes puntos:")
+    print("\tP1", p1)
+    print("\tP2", p2)
+    print("")
+    print("La distancia entre ellos es:", distancia)
